6871d70033d1a7ac00f41573d0d0249d98d9ca27
Aaryan-kumar-24 <aryankumarjanuary24@gmail.com>
2026-03-24 11:23:22 +0530

    added everything

--- suraksha/settings.py
+++ suraksha/settings.py
@@ -129,7 +129,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'amankumarjanuary@gmail.com'   # your gmail
-EMAIL_HOST_PASSWORD = 'gfbw caar bsan wqlm'  # ⚠️ change this now
+EMAIL_HOST_PASSWORD = 'gfbwcaaramanbsanwqlm'  # ⚠️ change this now
 
 
 # ================= TIMEZONE FIX =================
